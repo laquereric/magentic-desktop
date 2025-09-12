788128463d3e2a843e87ea767e6b08e30f89ea1d
Eric Laquer <LaquerEric@gmail.com>
2025-09-12 15:43:48 -0400

    part hier

--- image/instance.rb
+++ image/instance.rb
@@ -4,5 +4,6 @@ require "lubuntu_gui"
 
 if __FILE__ == $0
     instance = LubuntuGui::Instance.new(name: 'top.instance', source_file: __FILE__, directory: File.expand_path('..',__FILE__))
-    p instance.children
+    p "example: #{instance.children.first}"
+    p "count: #{instance.get_child_hier_flattened.count}"
 end
