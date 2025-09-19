b0ad8192aa38be7bfa9064e5e0bbcdc52f55409c
Eric Laquer <LaquerEric@gmail.com>
2025-09-19 10:13:13 -0400

    to ini file

--- container_scripts/catalog.rb
+++ container_scripts/catalog.rb
@@ -4,9 +4,12 @@ require "lubuntu_gui"
 require 'pp'
 
 if __FILE__ == $0
-    PP.pp(
-        LubuntuGui::Catalog.new(source_file: File.expand_path(__FILE__)).instance
-    )
+    file = __FILE__
+    parent = file.split("/")[0..-2].join("/")
+    catalog = LubuntuGui::Catalog.new(source_file: parent)
+    instance = catalog.instance
+    PP.pp(instance.catalog.parts)
+    catalog.get_item(entry_path: "collector/instance/")
     #p "example: #{instance.children.first}"
     #p "count: #{instance.children.count}"
 end
