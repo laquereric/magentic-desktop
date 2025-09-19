59fc1d896749af64a621ac78ce4d7c248d44362d
Eric Laquer <LaquerEric@gmail.com>
2025-09-19 17:36:27 -0400

    dirs better. No files

--- container_scripts/catalog.rb
+++ container_scripts/catalog.rb
@@ -3,13 +3,32 @@
 require "lubuntu_gui"
 require 'pp'
 
-if __FILE__ == $0
-    file = __FILE__
+def get_catalog_h(file:'scripts/catalog.rb')
     parent = file.split("/")[0..-2].join("/")
     catalog = LubuntuGui::Catalog.new(source_file: parent)
-    instance = catalog.instance
-    PP.pp(instance.catalog.parts)
-    catalog.get_item(entry_path: "collector/instance/")
-    #p "example: #{instance.children.first}"
-    #p "count: #{instance.children.count}"
+    {
+        catalog: catalog,
+        parent: parent,
+        instance: catalog.instance
+    }
 end
+
+def dump_catalog(file:'scripts/catalog.rb')
+    catalog_h = get_catalog_h(file:'scripts/catalog.rb')
+    PP.pp(catalog_h[:catalog].parts)
+end
+
+def catalog_test(file:'scripts/catalog.rb')
+    c = dump_catalog(file: file)[:catalog]
+    PP.pp(c.get_item(entry_path:'/collector_instance/users'))
+end
+
+if __FILE__ == $0
+    dump(file: __FILE__)
+end
+    # instance = catalog.instance
+    # PP.pp(instance.catalog.parts)
+    # catalog.get_item(entry_path: "collector/instance/")
+    
+    # p "example: #{instance.children.first}"
+    # p "count: #{instance.children.count}"
